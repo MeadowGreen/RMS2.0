43c9c59f5d2de4adc51d58e1e4ecfe9657ba877a
Meadow Green <meadowg.4543@gmail.com>
2021-02-26 14:22:28 -0700

    started faker.js file and wrote data

--- models/incident.js
+++ models/incident.js
@@ -4,11 +4,10 @@ var Schema = mongoose.Schema;
 
 var incidentSchema = new Schema({
     ir: { type: Number, required: true },
-    occurence_date: { type: Date, required: true },
-    occurence_time: { type: String, required: true },
+    occurrence_date: { type: Date, required: true },
+    occurrence_time: { type: String, required: true },
     incident_type: { type: String, required: true },
     location: { type: String, required: true0 },
-    location_entry: { type: String, required: true0 },
     narrative: { type: String, required: true00 },
     case_status: { type: String, required: true },
     officer_name: { type: String, required: true },
@@ -19,3 +18,5 @@ var incidentSchema = new Schema({
     supp: { type: String, required: true },
     serial_number: { type: Number, required: true }
 });
+
+
