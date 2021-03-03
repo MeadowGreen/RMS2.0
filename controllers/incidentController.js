152d9c4d15fd9df1effa3b840ca21bf1371181c7
Joel Mendivil <jmendivil171@west-mec.org>
2021-03-03 13:09:40 -0700

    Auto stash before merge of "main" and "origin/main"

--- controllers/incidentController.js
+++ controllers/incidentController.js
@@ -18,6 +18,7 @@ exports.incident_list = function (req, res, next) {
             //Successful, so render
             res.render('incident_list', { title: 'Incident List', incident_list: list_incidents });
         });
+    res.render('incident_list');
 };
 
 // Display detail page for a specific incident.
