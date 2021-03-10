7cc899d4bf718ab7374b14e36150363607fe8250
Meadow Green <meadowg.4543@gmail.com>
2021-03-10 15:26:50 -0700

    incident form save and post function

--- controllers/incidentController.js
+++ controllers/incidentController.js
@@ -1,8 +1,9 @@
 var Incident = require('../models/incident');
 var Person = require('../models/person');
 var Vehicle = require('../models/vehicle');
+const { body,validationResult } = require('express-validator');
 
-// Display the home page.
+// Display the home page.n
 
 exports.index = function (req, res, next) {
     res.render('index');
@@ -20,17 +21,6 @@ exports.incident_list = function(req, res, next) {
         });
 };
 
-
-exports.index = function (req, res) {
-    res.render('index');
-};
-
-// Display list of all incidents.
-exports.incident_list = function (req, res, next) {
-    res.send('NOT IMPLEMENTED: Incident list');
-};
-
- Stashed changes
 // Display detail page for a specific incident.
 exports.incident_detail = function (req, res) {
     res.send('NOT IMPLEMENTED: Incident detail: ' + req.params.id);
@@ -40,16 +30,46 @@ exports.incident_detail = function (req, res) {
 
 exports.incident_create_get = function (req, res) {
     res.render('incident_form');
-
-exports.incident_create_get = function (req, res) { 
-    res.send('NOT IMPLEMENTED: Incident create GET');
- Stashed changes
 };
 
-// Handle incident create on POST.
-exports.incident_create_post = function (req, res) {
-    res.send('NOT IMPLEMENTED: Incident create POST');
-};
+exports.incident_create_post = [ 
+    // Process request after validation and sanitization.
+    (req, res, next) => {
+     // Create incident object with escaped and trimmed data
+        var incident = new Incident(
+            {
+                ir: req.body.ir,
+                incident_type: req.body.incident_type,
+                occurrence_date: req.body.occurrence_date,
+                occurrence_time: req.body.occurrence_time,
+                location_address: req.body.location_address,
+                location_name: req.body.location_name,
+                narrative: req.body.narrative,
+                officer_name: req.body.officer_name,
+                officer_num: req.body.officer_num,
+                report_date: req.body.report_date,
+                report_time: req.body.report_time,
+            }
+        );
+
+        if (!errors.isEmpty()) {
+            // There are errors. Render form again with sanitized values/errors messages.
+            res.render('incident_form', { title: 'Create Incident', incident: incident, errors: errors.array() });
+            return;
+        }
+        else {
+            // Data from form is valid.
+
+            // Save incident.
+            incident.save(function (err) {
+                if (err) { return next(err); }
+                // Successful - redirect to new incident.
+                res.redirect(incident.url);
+            });
+        }
+    }
+
+];
 
 // Display incident delete form on GET.
 exports.incident_delete_get = function (req, res) {
--- models/incident.js
+++ models/incident.js
@@ -4,14 +4,14 @@ var Schema = mongoose.Schema;
 
 var IncidentSchema = new Schema({
     ir: { type: Number, required: true },
+    incident_type: { type: String, required: true },
     occurrence_date: { type: Date, required: true },
     occurrence_time: { type: String, required: true },
-    incident_type: { type: String, required: true },
-    location: { type: String, required: true },
+    location_address: { type: String, required: true },
     location_name: { type: String, required: true },
     narrative: { type: String, required: true },
     officer_name: { type: String, required: true },
-    officer_serial_number: { type: Number, required: true },
+    officer_num: { type: Number, required: true },
     report_date: { type: Date, required: true },
     report_time: { type: String, required: true }
 });
